782b97be5de89d3bbf2dba04c509bb948509c245
pjshub <pjaj2502@gmail.com>
2024-05-21 19:11:52 -0500

    comment in main.tf & bucket name change

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 ## This code works from Cloud Shell
-## Test for DragonFly
+### Test for DragonFly
 terraform {
   backend "gcs" {
-    bucket  = "tf-1-terraform-state-bucket"
-    prefix  = "tf-1-state-bucket/prod"  # Optional for organization
+    bucket = "tf-1-terraform-state-bucket"
+    prefix = "tf-1-state-bucket/prod" # Optional for organization
   }
 }
 provider "google" {
   credentials = file("../sakeys/tf-1-354422-key.json")
-  project = "tf-1-354422"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  project     = "tf-1-354422"
+  region      = "us-central1"
+  zone        = "us-central1-c"
   #Clone, Save, Change+, Stage, Commit, Push 
   ##
 }
@@ -29,14 +29,14 @@ resource "google_compute_instance" "terraform-pj-svc-4" {
   }
 }
 resource "google_storage_bucket" "bucket-1" {
-  name = "pj-my-bucket-1"
-  location = "US"
-  storage_class = "STANDARD"
+  name                        = "pj-my-bucket-1"
+  location                    = "US"
+  storage_class               = "STANDARD"
   uniform_bucket_level_access = true
 }
-resource "google_storage_bucket" "bucket-2" {
-  name = "pj-my-bucket-2"
-  location = "US"
-  storage_class = "STANDARD"
+resource "google_storage_bucket" "bucket-01" {
+  name                        = "pj-my-bucket-01"
+  location                    = "US"
+  storage_class               = "STANDARD"
   uniform_bucket_level_access = true
 }
